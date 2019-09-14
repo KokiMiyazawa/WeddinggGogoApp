5b6b6060b4174a7d62bb4c3506ee33ca106ecbc7
KokiMiyazawa <kokimiyazaworld@gmail.com>
2019-09-15 03:26:44 +0900

    20190915 ・初期画面 ・button追加 ・アラート追加 ・セグエでGOGOCHANCE画面に遷移

--- WeddinggGogoApp/ViewController.swift
+++ WeddinggGogoApp/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tappedEnter(_ sender: Any) {
+        
+        let alert: UIAlertController = UIAlertController(title: "本人確認", message: "あなたは、さとうさん、ですかい？", preferredStyle: UIAlertController.Style.alert)
+        
+        let selectYesAction: UIAlertAction = UIAlertAction(title: "YES", style: UIAlertAction.Style.default, handler: {
+            (action: UIAlertAction!) -> Void in
+            //YESボタン押下でGOGOCHANCE画面に遷移する
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.performSegue(withIdentifier: "toGogoChanceView", sender: nil)
+            }
+            print("YES")
+        })
+        
+        let selectNoAction: UIAlertAction = UIAlertAction(title: "NO", style: UIAlertAction.Style.default, handler: {
+            (action: UIAlertAction!) -> Void in
+            print("NO")
+        })
+        
+        alert.addAction(selectYesAction)
+        alert.addAction(selectNoAction)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
