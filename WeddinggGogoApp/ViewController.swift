79001fea6c42a39714b50886a8cf18f0ab704cbb
KokiMiyazawa <kokimiyazaworld@gmail.com>
2019-09-15 01:44:16 +0900

    Initial Commit

---
+++ WeddinggGogoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeddinggGogoApp
+//
+//  Created by Koki Miyazawa on 2019/09/15.
+//  Copyright © 2019 Koki_Miyazawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
